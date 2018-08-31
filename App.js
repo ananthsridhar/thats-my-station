f1e69090bb7ba1d99409b5955b18c484f24bb8e6
nanthsree22 <nanthsree22@gmail.com>
2018-08-31 22:13:26 +0530

    Initial Commit with Blue square

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import FormComponent from 'FormComponent';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MainContainer/>
+        </View>
+    );
+  }
+}
+
+class MainContainer extends React.Component {
+   render(){
+     return (
+       <View style={{flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',}}>
+        <FormComponent/>
+     </View>
+     )
+   }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f43',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ FormComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class FormComponent extends React.Component {
+  render() {
+    return (
+      <View>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+      </View>
+    );
+  }
+}
