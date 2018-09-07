2f73cebcd7daa27fd4a7cc4ef4d87d254830b500
nanthsree22 <nanthsree22@gmail.com>
2018-09-07 21:16:47 +0530

    Added MRTS Station,Chennai

--- FormComponent.js
+++ FormComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Alert,StyleSheet, Text, View,Dimensions,Picker,TouchableOpacity, ScrollView } from 'react-native';
-import { Overlay } from 'react-native-elements';
+import { Icon ,Overlay } from 'react-native-elements';
+
+import station_data from './resources/stationData.json'
 
 export default class FormComponent extends React.Component {
 
@@ -12,20 +14,21 @@ export default class FormComponent extends React.Component {
       whichModal : "",
       fromStation : "",
       toStation:"",
-      stationData : "",
-      fromStationList : "",
+      stationData : station_data,
+      fromStations : [...new Set(station_data)],
       toStationList : ""
     }
     this.onPress = this.onPress.bind(this);
     this.setStation = this.setStation.bind(this);
+    //console.log(station_data[0].station_name);
   }
 
   componentDidMount(){
-    this.getAllData();
+    //this.getAllData();
   }
 
   onPress(e){
-    console.log(e);
+    //console.log(e);
     if(e==="from")
     {
       this.setState({
@@ -61,11 +64,12 @@ export default class FormComponent extends React.Component {
         toStations : toL
       })
     }
-    else {
+    else
+    {
         this.setState({
           toStation : theStation
         })
-      }
+    }
   }
 
   render() {
@@ -74,15 +78,19 @@ export default class FormComponent extends React.Component {
     return (
       <View>
         <View style={{width: Dimensions.get('window').width, height:Dimensions.get('window').height, backgroundColor: 'powderblue'}} >
-          <View style={{padding:40}}>
+          <View style={{flex:1}}>
             <Text>Hello</Text>
+          </View>
+          <View style={{flex :1,padding:40}}>
             <View style={{padding:40}}>
               <TouchableOpacity
                 style={styles.button}
                 onPress={()=>this.onPress('from')}
                 name="from"
               >
-                <Text> {this.state.fromStation==""?"Touch Here":this.state.fromStation.station_name} </Text>
+              <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+                <Text> {this.state.fromStation==""?"SELECT ORIGIN":this.state.fromStation.station_name} </Text><Icon name='rowing'/>
+                </View>
               </TouchableOpacity>
             </View>
             <View style={{padding:40}}>
@@ -91,9 +99,11 @@ export default class FormComponent extends React.Component {
                 onPress={()=>this.onPress('to')}
                 name="to"
               >
-                <Text> {this.state.toStation==""?"Touch Here":this.state.toStation.station_name} </Text>
+                <Text> {this.state.toStation==""?"SELECT DESTINATION":this.state.toStation.station_name} </Text>
               </TouchableOpacity>
             </View>
+          </View>
+          <View style={{flex:1}}>
           </View>
         </View>
         <OverlayComponent
@@ -124,14 +134,6 @@ getAllData() {
           stationData: data,
           fromStations : data
         });
-        // data.map((station, id) => {
-        //   //console.log(station.station_name);
-        //   fromDd.push(
-        //     <option value={station.id} key={id}>
-        //       {station.station_name} {station.line_no}
-        //     </option>
-        //   );
-        // });
       });
   }
 
@@ -161,9 +163,6 @@ getAllData() {
 
 
 /*End Utility functions*/
-
-
-
 
 }
 
@@ -200,14 +199,14 @@ selectedItemReturn(sid){
       </Overlay>
     );
   }
-
 }
 
 
 class TouchableListItem extends React.Component {
   render(){
     return (
-      <TouchableOpacity style={{backgroundColor:'green',padding:10}} onPress={()=>this.props.selectItem(this.props.sid)}>
+      <TouchableOpacity style={{backgroundColor:'green',padding:10,borderWidth: 0.5,
+      borderColor: '#d6d7da'}} onPress={()=>this.props.selectItem(this.props.sid)}>
         <Text>{this.props.text}</Text>
       </TouchableOpacity>
     );
@@ -218,7 +217,10 @@ class TouchableListItem extends React.Component {
 const styles = StyleSheet.create({
   button: {
     alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 5
+    // backgroundColor: '#DDDDDD',
+    padding: 5,
+    borderRadius: 4,
+    borderBottomWidth: 2,
+    borderBottomColor: '#d6d7da'
   }
 });
